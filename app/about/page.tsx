b7bc11eb0bee7f50f91f1394b1bfede429a160f0
victoria <victoriapcavalcante@gmail.com>
2025-02-23 09:05:55 +0100

    about-us

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+import { notFound } from "next/navigation"
+import Image from "next/image"
+
+export default async function About() {
+    return (
+      <div className="space-y-8">
+        <div className="text-center space-y-4">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl">
+            About us
+          </h1>
+          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+          Integer gravida condimentum odio fermentum volutpat. Curabitur eu ipsum nec sapien iaculis semper. Quisque ultricies, ipsum non aliquam tincidunt, lacus ipsum luctus eros, at porta nibh mi sit amet urna. Nunc hendrerit purus dui, vitae pellentesque ante consequat id. Nunc a luctus dolor. Phasellus ut dolor finibus ligula rutrum laoreet quis id ex. Phasellus nec est et quam viverra fringilla in eu orci. Praesent ultrices, lacus id porta laoreet, nisl nisi auctor augue, quis auctor orci dolor in dolor. Nunc lobortis porttitor orci, sed iaculis augue tincidunt vitae. Donec ornare enim vitae ligula tempus fringilla. Donec posuere augue ut risus tincidunt, ut rhoncus nibh congue. Mauris eleifend lorem ut metus malesuada, eget lacinia nibh vestibulum. Praesent sed iaculis ligula. Donec accumsan convallis ante sit amet convallis. Suspendisse id maximus neque. Ut non pretium leo.
+          </p>
+        </div>
+                
+      </div>
+    )
+  
+}
